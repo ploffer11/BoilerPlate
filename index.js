a64ceaad11b1c757c12775e43d6b0f9b66566de8
Junho Kim <ploffer11@naver.com>
2020-01-28 17:19:05 +0900

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-mongoose.connect("mongodb+srv://ploffer11:1234@react-node-nm2xs.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-).then(() => console.log("DB connected")).catch(err => console.error(err));
+const config = require('./config/key');
+const { User } = require('./models/user');
 
+mongoose.connect(config.mongoURI,
+    { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("DB connected"))
+    .catch(err => {
+        console.log("DB connecting failed");
+        console.error(err);
+    });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userData) => {
+        console.log("In callback!");
+        if (err) {
+            return res.json({ success: false, err });
+        }
+
+        return res.status(200).json({
+            success: true
+        });
+    })
+});
 
 app.get('/', (req, res) => {
     res.send('Hello world');
